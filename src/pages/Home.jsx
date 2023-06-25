ca8bc3f63f37009282c5e830015ec38424246f5f
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-06-25 13:51:37 +0200

    p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import { useSelector } from 'react-redux';
 import { selectUser } from 'redux/auth/authSelectors';
 
 const Home = () => {
+
   const user = useSelector(selectUser);
   const name = user
     ? `${user.name}, welcome to your contact book`
     : "please register if you do not have an account to see your contact book";
 
   return (
-    <p style={{ paddingLeft: 200, paddingRight: 200 }}>Hello {name} </p>
+    <p style={{fontWeight: 500,    fontSize: 48,    textAlign: 'center'}}>Hello {name}🙋 </p>
   );
 };
 
